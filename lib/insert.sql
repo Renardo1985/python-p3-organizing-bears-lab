47ba3120ed1b8cf3e8d10498440aeea9482607fe
Renardo Williams <rennwill@gmail.com>
2023-07-30 23:06:24 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 1, 'M', 'Black', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 1, 'F', 'Purple', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 1, 'F', 'Pink', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 1, 'M', 'Green', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 1, 'F', 'Red', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'M', 'White', 'Happy', true);
